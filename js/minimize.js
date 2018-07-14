9b2c96f99f0aebd3fd6136612f566f9663807f97
vincentlaucsb <vincela9@gmail.com>
2018-07-13 19:38:12 -0700

    Pimped out ACE editor

---
+++ js/minimize.js
@@ -0,0 +1,42 @@
+function ClassChanger(
+    class_name,
+    trigger,
+    target,
+    minimize_text = "Minimize Preview",
+    restore_text = "Restore Preview"
+) {
+    this.trigger = trigger;
+    this.target = target;
+    this.class_name = class_name;
+    this.minimize_text = minimize_text;
+    this.restore_text = restore_text;
+    
+    trigger.innerHTML = minimize_text;
+    
+    trigger.addEventListener("click", function() {
+        if (target.classList.contains(class_name)) {
+            target.classList.remove(class_name);
+            trigger.innerHTML = minimize_text;
+        } else {
+            target.classList.add(class_name);
+            trigger.innerHTML = restore_text;
+        }
+    });
+}
+
+function Minimizer(
+    trigger,
+    target,
+    minimize_text = "Minimize Preview",
+    restore_text = "Restore Preview"
+    ) {
+        return new ClassChanger("minimized", trigger, target, minimize_text, restore_text);
+    }
+    
+function Fullscreen(
+    trigger,
+    target,
+    minimize_text = "Go Fullscreen",
+    restore_text = "Go Back") {
+        return new ClassChanger("fullscreen", trigger, target, minimize_text, restore_text);
+    }
